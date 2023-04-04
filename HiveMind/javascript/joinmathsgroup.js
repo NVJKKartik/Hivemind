3f1034df823e41d15a1fd03541c0cced462a9a8c
priyeshgupta14 <21bds050@iiitdwd.ac.in>
2023-04-04 13:06:04 +0530

    Frontend Changed

---
+++ HiveMind/javascript/joinmathsgroup.js
@@ -0,0 +1,4 @@
+const joinBtn = document.getElementById('joinBtn');
+  joinBtn.addEventListener('click', function() {
+    window.location.href = 'mathsgroup.html';
+  });
--- HiveMind/joinmathsgroup.js
+++
@@ -1,6 +0,0 @@
-const joinBtn = document.getElementById('joinBtn');
-  joinBtn.addEventListener('click', function() {
-    window.location.href = 'mathsgroup.html';
-  });
-
-
